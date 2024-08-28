3d71b6af45535675b027c4766faffefb398875bd
Wael Asmar <wasmar3301@gmail.com>
2024-08-28 16:34:51 +0300

    sorted but many operations

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: wasmar <wasmar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 15:04:33 by wasmar            #+#    #+#             */
-/*   Updated: 2024/08/28 12:29:23 by wasmar           ###   ########.fr       */
+/*   Updated: 2024/08/28 16:33:36 by wasmar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,38 +93,44 @@ int main(int argc, char **argv)
     t_stack *stack_b = NULL;
     pb(&stack_a,&stack_b);
     pb(&stack_a,&stack_b);
-
     while (count_list(stack_a) > 0)
     {
-    set_pos_and_median(stack_a);
-    set_pos_and_median(stack_b);
-    set_target_of_a(stack_a,stack_b);
-    find_cost(stack_a);
-    find_cost(stack_b);
-    add_cost_of_a_and_b(stack_a,stack_b);
-    t_stack *cheap = find_cheapest_node(stack_a);
-    if(cheap->median == 1 && cheap->target->median == 1)
-    {
-        rotate_a_and_b(&stack_a,&stack_a,cheap);
-    }
-    else if(cheap->median == 0 && cheap->target->median == 0)
-    {
-        reverse_rotate_a_and_b(&stack_a,&stack_b,cheap);
-    }
-    put_node_on_top_a(&stack_a,cheap);
-    put_node_on_top_b(&stack_b,cheap);
-    pb(&stack_a,&stack_b);
+        set_pos_and_median(stack_a);
+        set_pos_and_median(stack_b);
+        set_target_of_a(stack_a,stack_b);
+        find_cost(stack_a);
+        find_cost(stack_b);
+        add_cost_of_a_and_b(stack_a,stack_b);
+        t_stack *cheap = find_cheapest_node(stack_a);
+        if(cheap->median == 1 && cheap->target->median == 1)
+        {
+            rotate_a_and_b(&stack_a,&stack_b,cheap);
+        }
+        else if(cheap->median == 0 && cheap->target->median == 0)
+        {
+            reverse_rotate_a_and_b(&stack_a,&stack_b,cheap);
+        }
+        put_node_on_top_a(&stack_a,cheap);
+        put_node_on_top_b(&stack_b,cheap);
+        pb(&stack_a,&stack_b);
     }
     t_stack *max = find_max_pointer(stack_b);
     set_pos_and_median(stack_b);
     while (max ->position != 1)
     {
-        rrb(&stack_b,1);
-            set_pos_and_median(stack_b);
+        if(max ->median == 1)
+            rb(&stack_b,1);
+        else if(max ->median == 0)
+            rrb(&stack_b,1);
+        set_pos_and_median(stack_b);
 
     }
+    while (stack_b != NULL)
+    {
+        pa(&stack_b,&stack_a);
+    }
     
-    print_stack(stack_b);
+   // print_stack(stack_a);
     free_linked_list(stack_a);
    free(data);
 }
