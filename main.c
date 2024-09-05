daaf54de97f466ba09c3039f29d809b219509dd9
Wael Asmar <wasmar3301@gmail.com>
2024-09-05 11:53:07 +0300

    some fixs

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: wasmar <wasmar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 15:04:33 by wasmar            #+#    #+#             */
-/*   Updated: 2024/09/05 10:48:12 by wasmar           ###   ########.fr       */
+/*   Updated: 2024/09/05 11:51:24 by wasmar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -166,7 +166,7 @@ bool split_argument(char *argv, t_input *input, int **data)
     int error = 0;
     int ftatoi = 0;
     char **split = ft_split(argv, charset);
-    if(split == NULL)
+     if(split == NULL)
     {
         return(false);
     }
@@ -174,7 +174,7 @@ bool split_argument(char *argv, t_input *input, int **data)
    i = 0;
    if(errorr == false)
    {
-        for (int j = 0; split[j]; j++) // fix this 
+        for (int j = 0; split[j]; j++) // fix this
             free(split[j]);
         free(split);
     return false;
@@ -255,7 +255,17 @@ void one_argument_helper(t_input **input,  t_stack **stack_a,bool indicator,int 
         free(*input);
         free(*data);
         free_linked_list(*stack_a);
-        printf("Error3");
+        write(2,"Error\n",5);
+        exit(0);
+    }
+}
+void one_argument_helper1(t_input **input,  t_stack **stack_a,bool indicator,int **data)
+{
+    if(indicator == false)
+    {
+        free(*input);
+        free(*data);
+        free_linked_list(*stack_a);
         exit(0);
     }
 }
@@ -275,7 +285,16 @@ void one_argument(char *argv,t_stack **stack_a)
         exit(0);
     }
     (*stack_a) = create_list_a(data,input->input_count,0);
-    one_argument_helper(&input,stack_a,check_if_sorted(*stack_a),&data);
+    int i = count_list(*stack_a);
+    if(i == 1)
+    {
+        free(input);
+        free(data);
+        free_linked_list(*stack_a);
+        exit(0);
+    }
+    one_argument_helper1(&input,stack_a,check_if_sorted(*stack_a),&data);
+    
     one_argument_helper(&input,stack_a,check_dup(*stack_a),&data);
     free(data);
     free(input);
@@ -307,7 +326,6 @@ void many_arguments(t_stack **stack_a,char **argv,int argc)
         {
             free(data);
              free_linked_list(*stack_a);
-            write(2,"Error1",6);
             exit(0);
         }
                bool error1 = check_dup (*stack_a);
@@ -322,8 +340,6 @@ void many_arguments(t_stack **stack_a,char **argv,int argc)
 }
 int main(int argc, char **argv)
 {   
-    // int *data;
-    bool error = true;
     t_stack *stack_a;
     t_stack *stack_b = NULL;
     if(argc != 1)
@@ -331,42 +347,7 @@ int main(int argc, char **argv)
     if(argc == 2)
         one_argument(argv[1],&stack_a);  
     else if( argc >2)
-    {
         many_arguments(&stack_a,argv,argc);
-        // data = malloc((argc-1)*sizeof(int));
-        // error = create_array_with_input(argv,argc,&data);
-        // if(error == false)
-        // {
-        //     free(data);
-        //     write(2,"Error3",6);
-        //     exit(0);
-        // }
-        // error = check_numeric(argv,1);
-        // if(error == false)
-        // {
-        //     free(data);
-        //     write(2,"Error3",6);
-        //     exit(0);
-        // }
-    //    stack_a =  create_list_a(data,argc,1);
-    //    bool error2 = true; 
-    //      error2 = check_if_sorted(stack_a);
-    //     if(error2 == true)
-    //     {
-    //         free(data);
-    //          free_linked_list(stack_a);
-    //         write(2,"Error1",6);
-    //         exit(0);
-    //     }
-    //            bool error1 = check_dup (stack_a);
-    //     if(error1 == false)
-    //     {
-    //         free(data);
-    //         free_linked_list(stack_a);
-    //         write(2,"Error2",6);
-    //         exit(0);
-    //     }
-    }
      sort(stack_a,stack_b);
     }
     else
